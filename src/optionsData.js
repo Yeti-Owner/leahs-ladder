c276534bbe73981d9550b791f11533c367350a91
Callum <calrossmaclean@gmail.com>
2025-03-02 18:58:23 -0500

    Added real options
    no more placeholders

--- src/optionsData.js
+++ src/optionsData.js
@@ -1,87 +1,115 @@
-import image1 from './assets/1.png';
-import image2 from './assets/2.png';
-import image3 from './assets/3.png';
-import image4 from './assets/4.png';
-import image5 from './assets/5.png';
-import image6 from './assets/6.png';
-import image7 from './assets/7.png';
-import image8 from './assets/8.png';
-import image9 from './assets/9.png';
-import image10 from './assets/10.png';
-import image11 from './assets/11.png';
-import image12 from './assets/12.png';
+import minecraft from './assets/Minecraft.png';
+import terraria from './assets/Terraria.png';
+import stardew from './assets/Stardew.png';
+import nomanssky from './assets/NoMansSky.png';
+import roblox from './assets/Roblox.png';
+import worldwarz from './assets/WorldWarZ.png';
+import l4d2 from './assets/L4D2.png';
+import dinnerinamerica from './assets/DinnerInAmerica.png';
+import hadestown from './assets/HadesTown.png';
+import asteroidcity from './assets/AsteroidCity.png';
+import silentvoice from './assets/SilentVoice.png';
+import barbiedreamhouse from './assets/BarbieDreamhouse.png';
+import lemis from './assets/LesMiserables.png';
+import wendylucy from './assets/WendyLucy.png';
+import deathcure from './assets/DeathCure.png';
+import hauntinghour from './assets/HauntingHour.png';
 
 export const optionsData = [
   {
-    title: "Option 1",
-    description: "This is the first option",
-    image: image1,
+    title: "Minecraft",
+    description: "We play some craft, you build some pink stuff and I do some mining,",
+    image: minecraft,
     tags: ["games"]
   },
   {
-    title: "Option 2",
-    description: "This is the second option",
-    image: image2,
-    tags: ["movies"]
-  },
-  {
-    title: "Option 3",
-    description: "This is the third option",
-    image: image3,
-    tags: ["episodes"]
-  },
-  {
-    title: "Option 4",
-    description: "This is the fourth option",
-    image: image4,
+    title: "Terraria",
+    description: "We don't play this much, but there's plenty to do + some bosses.",
+    image: terraria,
     tags: ["games"]
   },
   {
-    title: "Option 5",
-    description: "This is the fifth option",
-    image: image5,
-    tags: ["movies"]
-  },
-  {
-    title: "Option 6",
-    description: "This is the sixth option",
-    image: image6,
-    tags: ["episodes"]
-  },
-  {
-    title: "Option 7",
-    description: "A thrilling adventure game",
-    image: image7,
+    title: "Stardew Valley",
+    description: "We can run up the Slay farm and pet the cat.",
+    image: stardew,
     tags: ["games"]
   },
   {
-    title: "Option 8",
-    description: "A classic romantic movie",
-    image: image8,
-    tags: ["movies"]
-  },
-  {
-    title: "Option 9",
-    description: "An intense drama episode",
-    image: image9,
-    tags: ["episodes"]
-  },
-  {
-    title: "Option 10",
-    description: "A fantasy strategy game",
-    image: image10,
+    title: "No Man's Sky",
+    description: "I still wana play this, but the performance bug is annoying...",
+    image: nomanssky,
     tags: ["games"]
   },
   {
-    title: "Option 11",
-    description: "A sci-fi blockbuster movie",
-    image: image11,
+    title: "Roblox",
+    description: "There are trillions of games including Flower Sim that you liked.",
+    image: roblox,
+    tags: ["games"]
+  },
+  {
+    title: "World War Z",
+    description: "We haven't played this yet actually, but I wanna make sure there's time for it.",
+    image: worldwarz,
+    tags: ["games"]
+  },
+  {
+    title: "Left 4 Dead 2",
+    description: "We actually both have this game but you never played and I only played once.",
+    image: l4d2,
+    tags: ["games"]
+  },
+  {
+    title: "Dinner In America",
+    description: "It's a RomCom about a Punk Band and a fangirl who go on a roadtrip.",
+    image: dinnerinamerica,
     tags: ["movies"]
   },
   {
-    title: "Option 12",
-    description: "A mystery thriller episode",
-    image: image12,
+    title: "Hadestown (Pro Shot)",
+    description: "Technically it isn't released yet so.....",
+    image: hadestown,
+    tags: ["movies"]
+  },
+  {
+    title: "Asteroid City",
+    description: "My personal fav Wes Anderson movie, I think you'll like it but not love it.",
+    image: asteroidcity,
+    tags: ["movies"]
+  },
+  {
+    title: "A Silent Voice",
+    description: "This one is marked sad, I haven't seen it yet but it's on my list.",
+    image: silentvoice,
+    tags: ["movies"]
+  },
+  {
+    title: "Barbie Life in the Dreamhouse",
+    description: "This is a series but 2 seasons is ~1.5 hours so we can treat it as a show or movie.",
+    image: barbiedreamhouse,
+    tags: ["episodes", "movies"]
+  },
+  {
+    title: "Les Miserables",
+    description: "I don't remember this besides the fact it was boring lowk, but it's worth rewatching.",
+    image: lemis,
+    tags: ["movies"]
+  },
+  {
+    title: "Wendy and Lucy",
+    description: "THIS MOVIE IS SAD. Not as sad as Only the Brave though.",
+    image: wendylucy,
+    tags: ["movies"]
+  },
+  {
+    title: "Maze Runner Death Cure",
+    description: "The final movie to watch for the trilogy.",
+    image: deathcure,
+    tags: ["movies"]
+  },
+  {
+    title: "R.L. Stine's Haunting Hour",
+    description: "There's still plenty of episodes to watch.",
+    image: hauntinghour,
     tags: ["episodes"]
   },
 ];
